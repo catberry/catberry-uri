99299e79308b2e8f7b04f8fe1f2becaa16308b59
Denis Rechkunov <denis.rechkunov@gmail.com>
2014-10-30 16:43:52 +0600

    Fix index.js.

--- index.js
+++ index.js
@@ -29,3 +29,10 @@
  */
 
 'use strict';
+
+module.exports = {
+	URI: require('./lib/URI'),
+	Authority: require('./lib/Authority'),
+	UserInfo: require('./lib/UserInfo'),
+	Query: require('./lib/Query')
+};
